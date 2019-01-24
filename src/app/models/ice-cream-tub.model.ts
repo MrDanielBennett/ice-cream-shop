aaa784cf26d743b18f05828c9c8eb7395c18e38a
Daniel Bennett and Michael Brown <michael@>
2019-01-24 12:02:12 -0800

    make ingredients an object

--- src/app/models/ice-cream-tub.model.ts
+++ src/app/models/ice-cream-tub.model.ts
@@ -1,5 +1,6 @@
+import { Content } from './content.model';
 class IceCreamTub {
-  constructor(public name: string, public price: number, public flavors: string[], public brand: string){}
+  constructor(public name: string, public price: number, public contents: Content[], public brand: string){}
   displayPrice() {
     return ((Math.ceil(this.price*100))/100).toFixed(2);
   }
