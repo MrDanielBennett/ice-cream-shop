4f84ef3fc0977f3c4557625afa389c40884ab95e
Daniel Bennett and Michael Brown <daniel@>
2019-01-24 10:48:05 -0800

    add ice cream information in html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IceCreamTub } from './models/ice-cream-tub.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  iceCreamTubs: IceCreamTub[] = [
+    new IceCreamTub('Vanilla', 3.50, ['Vanilla'], 'Umpqua'),
+    new IceCreamTub('Chocolate', 3.50, ['Chocolate'], 'Tillamook'),
+    new IceCreamTub('Strawberry', 3.50, ['Strawberry'], 'Tillamook'),
+    new IceCreamTub('Neopolitan', 3.50, ['Chocolate','Vanilla','Strawberry'], 'Tillamook'),
+  ];
+
 }
