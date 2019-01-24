d9be9a9aeeedf1aaf957622bf5b595c84a71c6cb
Daniel Bennett and Michael Brown <daniel@>
2019-01-24 12:03:38 -0800

    implement editing funcitonality

--- src/app/ice-cream-editor/ice-cream-editor.component.ts
+++ src/app/ice-cream-editor/ice-cream-editor.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { IceCreamTub } from '../models/ice-cream-tub.model';
 
 @Component({
   selector: 'app-ice-cream-editor',
   templateUrl: './ice-cream-editor.component.html',
   styleUrls: ['./ice-cream-editor.component.css']
 })
-export class IceCreamEditorComponent implements OnInit {
+export class IceCreamEditorComponent {
+  @Input() childIceCreamTubs: IceCreamTub[];
+  @Input() employeeAccess: boolean;
+  @Output() clickDone = new EventEmitter();
 
   constructor() { }
 
-  ngOnInit() {
+  finishEditing() {
+    this.employeeAccess = false;
+    this.clickDone.emit();
   }
 
 }
